680fc4969cbee57109f1c16cff10500afeccf6bb
rooland98 <rooland98@gmail.com>
2022-05-16 21:13:52 +0200

    add route and page to create new campground

--- index.js
+++ index.js
@@ -15,6 +15,8 @@ async function main() {
   });
 }
 
+app.use(express.urlencoded({extended: true}));
+
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
 
@@ -27,10 +29,21 @@ app.get('/campgrounds', async (req,res) =>{
     res.render('campgrounds/index', {campgrounds});
 });
 
+app.get('/campgrounds/new', (req,res) =>{
+    res.render('campgrounds/new');
+});
+
+app.post('/campgrounds', async (req,res) => {
+    const campground = new Campground(req.body.campground);
+    await campground.save();
+    res.redirect(`/campgrounds/${campground._id}`);
+});
+
 app.get('/campgrounds/:id', async (req,res) =>{
     const campground = await Campground.findById(req.params.id);
     res.render('campgrounds/show', {campground});
 });
+
 
 app.listen(4444, () =>{
     console.log("Running on port 4000...")
