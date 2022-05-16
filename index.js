c0234134595cd7b8b54f7ef33592cd5a353cf683
rooland98 <rooland98@gmail.com>
2022-05-16 20:49:06 +0200

    add first campground template with newly created camp data

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const { default: mongoose } = require('mongoose');
 const app = express();
 const path = require('path');
 const Campground = require('./models/campground');
+const ejs = require('ejs');
 
 
 main().catch(err => console.log(err));
@@ -19,6 +20,11 @@ app.set('views', path.join(__dirname, 'views'));
 
 app.get("/", (req,res)=>{
     res.render('home');
+});
+
+app.get('/campgrounds', async (req,res) =>{
+    const campgrounds = await Campground.find({});
+    res.render('campgrounds/index', {campgrounds});
 });
 
 app.listen(4444, () =>{
