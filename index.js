4e3586a75d29901986576152ce6d71ca8b48057b
rooland98 <rooland98@gmail.com>
2022-05-16 20:19:03 +0200

    fix mongoose connection issues

--- index.js
+++ index.js
@@ -2,18 +2,14 @@ const express = require('express');
 const { default: mongoose } = require('mongoose');
 const app = express();
 const path = require('path');
+const Campground = require('./models/campground');
 
-mongoose.connect('mongodb://localhost:27017/yelp-camp', {
-    useNewUrlParser: true,
-    useCreateIndex: true,
-    useUnifiedTopology: true
-});
 
-const db = mongoose.connection;
-db.on("error", console.error.bind(console, "connection error:"));
-db.once("open", () =>{
-    console.log("Database connected");
-})
+main().catch(err => console.log(err));
+
+async function main() {
+  await mongoose.connect('mongodb://127.0.0.1:27017/test');
+}
 
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
@@ -22,10 +18,11 @@ app.get("/", (req,res)=>{
     res.render('home');
 });
 
-app.get('*', (req,res) =>{
-    res.send("Error page not found")
+app.get('/make-campground', async (req,res) =>{
+    const camp = new Campground({title: 'Lort peak'});
+    await camp.save();
+    res.send(camp);
 });
-
 
 app.listen(4444, () =>{
     console.log("Running on port 4000...")
