11a59db58ae6c9689669e2dff35c59e2788f4bcf
rooland98 <rooland98@gmail.com>
2022-05-16 19:48:32 +0200

    add ejs configuration and view template folder config

--- index.js
+++ index.js
@@ -1,7 +1,17 @@
 const express = require('express');
 const app = express();
 
+app.set('view engine', 'ejs');
+app.get("/", (req,res)=>{
+    console.log("Request recieved!");
+});
 
-app.listen(4000, () =>{
+
+app.get('*', (req,res) =>{
+    res.send("Error page not found")
+});
+
+
+app.listen(4444, () =>{
     console.log("Running on port 4000...")
 });
