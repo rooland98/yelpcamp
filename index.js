0dc577f54677fc4c022e83f2632c87bbe77c0599
rooland98 <rooland98@gmail.com>
2022-05-16 20:53:21 +0200

    add id lookup route for each campground

--- index.js
+++ index.js
@@ -27,6 +27,11 @@ app.get('/campgrounds', async (req,res) =>{
     res.render('campgrounds/index', {campgrounds});
 });
 
+app.get('/campgrounds/:id', async (req,res) =>{
+    const campground = await Campground.findById(req.params.id);
+    res.render('campgrounds/show', {campground});
+});
+
 app.listen(4444, () =>{
     console.log("Running on port 4000...")
 });
