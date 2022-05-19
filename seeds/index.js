35d84d19b7d79bbe9c4cca02cc1bd1f306e6b778
rooland98 <rooland98@gmail.com>
2022-05-19 19:57:20 +0200

    add unsplash collection api to get random pictures

--- seeds/index.js
+++ seeds/index.js
@@ -21,7 +21,8 @@ const seedDB = async () => {
         const random1000 = Math.floor(Math.random() * 1000);
         const camp = new Campground({
             location: `${cities[random1000].city}, ${cities[random1000].state}`,
-            title: `${sample(descriptors)} ${sample(places)}`
+            title: `${sample(descriptors)} ${sample(places)}`,
+            image: 'https://source.unsplash.com/collection/483251'
         })
         await camp.save();
     }
