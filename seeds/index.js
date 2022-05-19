0edaae53bf020e540bf11720bd1dc9cd7e22f733
rooland98 <rooland98@gmail.com>
2022-05-19 20:02:30 +0200

    add img and description display to show.ejs

--- seeds/index.js
+++ seeds/index.js
@@ -19,10 +19,13 @@ const seedDB = async () => {
     await Campground.deleteMany({});
     for (let i = 0; i < 50; i++) {
         const random1000 = Math.floor(Math.random() * 1000);
+        const randomPrice = Math.floor(Math.random()*20) + 10;
         const camp = new Campground({
             location: `${cities[random1000].city}, ${cities[random1000].state}`,
             title: `${sample(descriptors)} ${sample(places)}`,
-            image: 'https://source.unsplash.com/collection/483251'
+            image: 'https://source.unsplash.com/collection/483251',
+            description: 'Quis aute et deserunt do ad laborum. Dolor consectetur cupidatat labore laborum esse aute consequat dolor minim et Lorem magna quis dolore. Aute laborum Lorem fugiat enim consectetur irure duis in proident excepteur in sunt mollit. Aliquip sunt consequat amet excepteur. Veniam eu laborum tempor nulla qui cupidatat enim nulla. Ipsum nulla ex amet cupidatat incididunt dolor.',
+            price: randomPrice
         })
         await camp.save();
     }
